3acdd85cd57c3c9d6a49f8df630320cf82c5ef52
Stephanie Guzman <sguzman247@gmail.com>
2022-06-13 19:51:12 -0500

    Update determineWinner function to accept conditionals of mythicals game

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var game = new Game();
-//query selectors
+
 var chooseClassicGame = document.querySelector(".classic-choice-box");
 var chooseDifficultGame = document.querySelector(".difficult-choice-box");
 var chosenFightersContainer = document.querySelector(".chosen-fighters-container");
@@ -11,7 +11,6 @@ var subheader = document.querySelector(".subheader")
 var personScore = document.querySelector("#personScore")
 var computerScore = document.querySelector("#computerScore")
 
-// event listeners
 chooseClassicGame.addEventListener("click", showClassicGame);
 classicFightersContainer.addEventListener("click", function(event) {
   selectClassicFighter(event)
@@ -22,7 +21,6 @@ difficultFightersContainer.addEventListener("click", function(event) {
 });
 changeGameButton.addEventListener("click", changeGame)
 
-//helper functions
 function show(element) {
   element.classList.remove("hidden");
 };
@@ -31,14 +29,13 @@ function hide(element) {
   element.classList.add("hidden");
 };
 
-// triggered by event listener
 function showClassicGame() {
   game.gameType = "classic"
   hide(gameTypes)
   show(classicFightersContainer);
   subheader.innerText = `Choose your fighter!`
 };
-// triggered by event listener
+
 function selectClassicFighter(event) {
   var personFighter = event.target.id;
   if (event.target.id) {
@@ -71,11 +68,13 @@ function selectDifficultFighter(event) {
 
 function randomizeComputerFighter() {
   if (game.gameType === "classic") {
-    game.computer.fighter = game.classicFighters[Math.floor(Math.random() * game.classicFighters.length)];
+    game.computer.fighter = game.classicFighters[Math.floor(Math.random()
+      * game.classicFighters.length)];
     console.log(game.computer.fighter);
     return game.computer.fighter
   } else if (game.gameType === "difficult") {
-    game.computer.fighter = game.difficultFighters[Math.floor(Math.random() * game.difficultFighters.length)];
+    game.computer.fighter = game.difficultFighters[Math.floor(Math.random()
+      * game.difficultFighters.length)];
     console.log(game.computer.fighter);
     return game.computer.fighter
   }
@@ -89,8 +88,10 @@ function showFighters() {
   show(changeGameButton);
   chosenFightersContainer.innerHTML = `
   <section class="fighters">
-    <img src="assets/${game.person.fighter}.png" alt="laptop" width="100" height="100">
-    <img src="assets/${game.computer.fighter}.png" alt="laptop" width="100" height="100">
+    <img src="assets/${game.person.fighter}.png" id="${game.person.fighter}"
+      alt="${game.person.fighter}" width="200" height="200">
+    <img src="assets/${game.computer.fighter}.png" id="${game.computer.fighter}"
+      alt="${game.computer.fighter}" width="200" height="200">
   </section>`
   game.determineWinner();
   console.log("Hello!");
@@ -119,17 +120,3 @@ function changeGame() {
   hide(difficultFightersContainer);
   subheader.innerText = `Choose Your Game!`
 };
-
-
-// if the user clicks on the classic game, then the user will be displayed
-// a list of characters associated with that changeGameType
-// the user will then be able to select the character of their choice
-
-
-// if the user clicks on the difficult game, then the user will be displayed
-// a list of characters associated with that changeGameType
-// the user will then be able to select the character of their choice
-//pull in .game.gameType from main.js (catch w/ event listener)
-// function chooseDifficultGame(event) {
-//
-// }
--- player.js
+++ player.js
@@ -9,11 +9,4 @@ class Player {
   addWin() {
     this.wins ++
   }
-}
-
-
-  // the takeTurn function can be thought of like a Submit
-  // button on your form.
-  // When the player chooses their action,
-  // they need to invoke takeTurn to submit their action to
-  // the Game class
+};
